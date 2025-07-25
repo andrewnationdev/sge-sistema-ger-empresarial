4d7ef1748220100c4df29d3ecd8ce89b8ffde30b
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2025-07-25 17:35:40 +0000

    adicionar porta padrão

--- db/db.ts
+++ db/db.ts
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
   host: process.env.DATABASE_HOST,      
   user: process.env.DATABASE_USER, 
-  //
+  //Se não houver porta, a padrão será usada
   port: Number(process.env.DATABASE_PORT) || 3306,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
