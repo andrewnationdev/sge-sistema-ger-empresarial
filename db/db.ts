05a4da60575353a86384f6ded72857c03e0887b7
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2025-07-25 17:31:40 +0000

    adicionar porta

--- db/db.ts
+++ db/db.ts
@@ -3,6 +3,7 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
   host: process.env.DATABASE_HOST,      
   user: process.env.DATABASE_USER, 
+  port: Number(process.env.DATABASE_PORT) || 3306,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   waitForConnections: true,
