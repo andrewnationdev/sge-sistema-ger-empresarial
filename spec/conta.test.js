4d697b1af385b62eae12c6abce84d232de473bfc
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2025-08-27 23:17:10 +0000

    endpoint teste

--- spec/conta.test.js
+++ spec/conta.test.js
@@ -78,7 +78,7 @@ describe('Fluxo de Alteração de Tela de Usuário', () => {
 
         expect(u_data).toHaveProperty('id');
 
-        const req_alterar_dados = await fetch(`https://sge-sistema-ger-empresarial.vercel.app/api/gerenciar_conta?id=${u_data.id}`, {
+        const req_alterar_dados = await fetch(`https://sge-sistema-ger-empresarial.vercel.app/api/gerenciar_contas?id=${u_data.id}`, {
             method: "PUT",
             headers: {
                 'Content-Type': 'application/json',
